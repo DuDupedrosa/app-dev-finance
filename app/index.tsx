4ea5d441df34b9874dc5b7d258e9c05e4de11032
Eduardo Pedrosa <eduardobaptista66@gmail.com>
2024-10-09 17:52:45 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function homePage() {
+  return (
+    <SafeAreaView style={style.safeArea}>
+      <View>
+        <Text>HOME</Text>
+        <Link href={"/login"}>vamos para o login</Link>
+        <Button mode="contained" buttonColor="#1f2" icon={"camera"}>
+          {" "}
+          VAMOS?
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const style = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
---
+++ app/login.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function LoginPage() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>LOGIN EM</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
