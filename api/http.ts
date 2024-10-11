1be10dedfbeb30858705b25e7f103f8cfd786695
Eduardo Pedrosa <eduardobaptista66@gmail.com>
2024-10-11 14:53:07 -0300

    instalando o axios + criando instance

---
+++ api/http.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+const apiUrl = process.env.EXPO_PUBLIC_API_URL;
+
+export const http = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
