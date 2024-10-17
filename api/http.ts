ee2ce5786ac78f70b4f13644e9e9375afcc6da3a
Eduardo Pedrosa <eduardobaptista66@gmail.com>
2024-10-17 11:56:19 -0300

    criando interceptor 401

--- api/http.ts
+++ api/http.ts
@@ -1,3 +1,5 @@
+import { router } from "expo-router";
+
 import axios from "axios";
 const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 export const http = axios.create({
@@ -6,3 +8,22 @@ export const http = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+http.interceptors.response.use(
+  (response) => {
+    // Caso a requisição seja bem-sucedida
+    return response;
+  },
+  (error) => {
+    if (
+      error.response &&
+      error.response.status === axios.HttpStatusCode.Unauthorized
+    ) {
+      // Redireciona para a tela de login quando o status for 401 (não autorizado)
+      router.push("/auth");
+    }
+
+    // Retorna o erro para que possa ser tratado normalmente em outros lugares
+    return Promise.reject(error);
+  }
+);
